66d9022372092e6eb8d02d72c25bdf9663a56f9c
AnupamaaTodki <todkianupama@gmail.com>
2022-09-21 12:07:30 +0530

    Refracted:finding size of the Binary search tree

--- BinaryTree.java
+++ BinaryTree.java
@@ -35,7 +35,16 @@ public class BinaryTree<K extends Comparable <K>> {
 	binary.add(56);
 	binary.add(30);
 	binary.add(70);
-	
+	binary.add(22);
+	binary.add(40);
+	binary.add(60);
+	binary.add(95);
+	binary.add(11);
+	binary.add(3);
+	binary.add(16);
+	binary.add(65);
+	binary.add(63);
+	binary.add(67);
 	int bstSize = binary.getSize();
 	System.out.println("Size of binary tree is : "+bstSize);
 	
